7bccaaca743157bd3c037c3d847dcc7006237596
DanielVerano <alum.dveranom.2021@iesalixar.org>
2023-02-06 13:18:15 +0100

    Rutas de Usuario y Libro, View de Login

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ mongoose.set('strictQuery', false);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const librosRouter = require('./routes/libros');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/libros', librosRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'App Prestamos Libros' });
+})
 
 module.exports = router;
