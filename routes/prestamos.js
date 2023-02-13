21bf01167ca8975ba27dc7cd9641be267e5c3a3f
ClaudiaYLopezLafita <claudiayll94@gmail.com>
2023-02-13 21:01:32 +0100

    solution populate

--- routes/prestamos.js
+++ routes/prestamos.js
@@ -8,12 +8,22 @@ var Libro = require('../models/Libro.js');
 var Prestamo = require('../models/Prestamo.js');
 var User = require('../models/Usuario.js');
 
-// GET del listado de prestamos ordenados por fecha de publicación
-// mostramos el nombre del usuario y libro
+// GET del listado de prestamos ordenados por fecha de publicación -- FUNCIONA
+//.populate('usuarioID',{_id:0,username:1}).populate('libroID',{_id:0,titulo:1})
 router.get('/', (req, res, next) => {
     Prestamo.find()
     .sort('-fechaRetirada')
-    .populate('usuarioID',{_id:0,username:1}).populate('libroID',{_id:0,titulo:1})
+    .populate(
+        [{
+            path: 'usuarioID',
+            model: 'User',
+            select: '-_id username' //Fields you want to return in this populate
+        }, {
+            path: 'libroID',
+            model: 'Libro',
+            select: '-_id titulo' //Fields you want to return in this populate
+        }]
+    )
     .exec(function(err, prestamos) {
         if (err) res.status(500).send(err);
         else res.status(200).json(prestamos);
@@ -24,8 +34,17 @@ router.get('/', (req, res, next) => {
 router.get('/all/:id', function(req, res, next) {
     console.log(req.params.id)
     Prestamo.find({ 'usuarioID':req.params.id})
-    .populate('usuarioID',{_id:0,username:1})
-    .populate('libroID',{_id:0,titulo:1})
+    .populate(
+        [{
+            path: 'usuarioID',
+            model: 'User',
+            select: '-_id username' //Fields you want to return in this populate
+        },{
+            path: 'libroID',
+            model: 'Libro',
+            select: '-_id titulo' //Fields you want to return in this populate
+        }]
+    )
     .exec(function(err, prestamos){
         console.log(prestamos)
         if (err) res.status(500).send(err);
@@ -35,26 +54,36 @@ router.get('/all/:id', function(req, res, next) {
 
 // GET de todos los prestamos de un libro dado (identificado por su Id)
 router.get('/all/:id', function(req, res, next) {
+    //¿como poner find si esta dentro de un array?
     Prestamo.find({ 'libroID': req.params.id}).sort('-fechaRetirada')
-    .populate('libroID',{_id:0,titulo:1})
-    .populate('usuarioID',{_id:0,fullname:1})
-    .exec(function(err, posts){
+    .populate(
+        [{
+            path: 'usuarioID',
+            model: 'User',
+            select: '-_id username' //Fields you want to return in this populate
+        }, {
+            path: 'libroID',
+            model: 'Libro',
+            select: '-_id titulo' //Fields you want to return in this populate
+        }]
+    )
+    .exec(function(err, prestamos){
         if (err) res.status(500).send(err);
-        else res.status(200).json(posts);
+        else res.status(200).json(prestamos);
     });
 });
 
 //GET prestamos dependiendo del estado pasando por url el parámetro
 //usando querystrings
 router.get('/', function(req, res, next){
-    let estado = req.params.estado;
+    let estado = req.query.estado;
     console.log(estado);
     Prestamo.find({'estado':estado}).exec()
         .then(prestamos => res.status(200).json(prestamos))
         .catch(err => res.status(500).json({ message: err }))
 })
 
-// POST de un nuevo prestamo
+// POST de un nuevo prestamo -- FUNCIONA
 router.post('/', function(req, res, next) {
     //comprobamos que el usuario existe
     User.findById(req.body.usuarioID, function(err, userinfo) {
@@ -83,7 +112,7 @@ router.post('/', function(req, res, next) {
     });
 });
 
-// PUT: actualización de un prestamo existente (identificado por su Id)
+// PUT: actualización de un prestamo existente (identificado por su Id) -- FUNCIONA
 router.put('/:id', function(req, res, next) {
     Prestamo.findByIdAndUpdate(req.params.id,req.body, function(err,  prestamoinfo) {
         if (err) res.status(500).send(err);
@@ -91,7 +120,7 @@ router.put('/:id', function(req, res, next) {
     });
 });
 
-// DELETE de un prestamo existente (identificado por su Id)
+// DELETE de un prestamo existente (identificado por su Id) -- FUNCIONA
 router.delete('/:id', function(req, res, next) {
     Prestamo.findByIdAndDelete(req.params.id, function(err, prestamoinfo) {
         if (err) res.status(500).send(err);
