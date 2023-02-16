da8b4a9dec3bc2288669eadfabb406560ce8aab6
ClaudiaYLopezLafita <claudiayll94@gmail.com>
2023-02-16 13:40:23 +0100

    vista y vinculacion de servicio insert libro

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,9 @@ router.get('/register', (req, res) => {
   res.render('register', {title: 'Register'});
 })
 
+// Ruta para el register de usuario
+router.get('/registerLibro', (req, res) => {
+  res.render('registerLibro', {title: 'Register Libro'});
+})
+
 module.exports = router;
